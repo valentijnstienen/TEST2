c7494462266909a82dad9aab467a67d8292e87a0
Valentijn Stienen <43643917+valentijnstienen@users.noreply.github.com>
2020-03-26 21:09:23 +0100

    Delete bokehserverextension.py

--- binder/bokehserverextension.py
+++
@@ -1,7 +0,0 @@
-from subprocess import Popen
-
-def load_jupyter_server_extension(nbapp):
-"""serve the bokeh-app directory with bokeh server"""
-Popen(["bokeh", "serve", "bokeh-app", "--allow-websocket-origin=*"])
-
-#/proxy/5006/bokeh-app
